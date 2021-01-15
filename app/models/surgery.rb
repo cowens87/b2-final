558b0db09f45fd948d248b30fbfd09bfee864fd7
Cydnee Owens <cks410@yahoo.com>
2021-01-15 10:34:10 -0700

    add user story 2 model tests

--- app/models/surgery.rb
+++ app/models/surgery.rb
@@ -2,7 +2,7 @@ class Surgery < ApplicationRecord
   has_many :doctor_surgeries
   has_many :doctors, through: :doctor_surgeries
 
-  def self.find_others_by_day
-    where(day_of_week: 'Thursday')
+  def self.find_others_by_day(day)
+    where(day_of_week: day)
   end
 end
